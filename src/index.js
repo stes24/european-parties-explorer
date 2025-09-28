de0b595cef90f5a3a6650b6d098e777038562bfd
stes24 <onafets24@gmail.com>
2025-09-28 16:33:55 +0200

    Simple data loading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './index.scss'
+import * as d3 from 'd3'
 
-console.log('Hello World!')
+async function init () {
+  await loadData()
+}
+
+async function loadData () {
+  try {
+    const data = await d3.csv('./merged_dataset_with_mds.csv')
+    console.debug(`Data loaded (${data.length} rows)`)
+  } catch (e) {
+    console.error('Error loading data\n', e)
+  }
+}
+
+init()
