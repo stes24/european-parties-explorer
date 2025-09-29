911c85231bbb423f4c458be0a9a4182a719430f5
stes24 <onafets24@gmail.com>
2025-09-29 12:31:22 +0200

    Loading for dist version

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ async function init () {
 
 async function loadData () {
   try {
-    const data = await d3.csv('./merged_dataset_with_mds.csv')
-    console.debug(`Data loaded (${data.length} rows)`)
+    const data = await d3.csv('./assets/merged_dataset_with_mds.csv') // Remote execution (dist)
+    console.debug(`Data loaded from assets (${data.length} rows)`)
   } catch (e) {
-    console.error('Error loading data\n', e)
+    try {
+      const data = await d3.csv('./merged_dataset_with_mds.csv') // Local execution
+      console.debug(`Data loaded from public (${data.length} rows)`)
+    } catch (e) {
+      console.error('Error loading data\n', e)
+    }
   }
 }
 
